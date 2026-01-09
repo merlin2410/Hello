b6c63e4dc95a794fb102956a6566f374f0e5ca5c
Merlin Mon Mathew <merlinmonmathew@gmail.com>
2026-01-09 10:40:44 +0530

    added Hello CET

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld{
 	public static void main(String[] args){
 
 
-		System.out.print("Hello World");	
+		System.out.println("Hello World");
+		System.out.println("Hello CET!");	
 
 
 	}	
